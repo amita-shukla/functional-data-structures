86e2de6bbd343105aa02bd3dd58567470c856d3a
amita-shukla <amitashukla0906@gmail.com>
2019-05-08 01:50:46 +0530

    implement List

--- src/List.hs
+++ src/List.hs
@@ -1,4 +1,20 @@
 module List where
 
-data List a = Nil | a : (List a)
+import Prelude hiding ((++))
 
+data List a = Nil | Cons a (List a)
+  deriving Show
+
+(++) :: List a -> List a -> List a
+(++) Nil ys = ys
+-- (++) xs Nil = xs
+(++) (Cons h t) ys = Cons h (t ++ ys)
+
+update :: List a -> Int -> a -> List a
+update Nil _ _ = Nil
+update (Cons h t) i x =
+  if i == 0 then
+    Cons x t
+  else
+    -- update t (i-1) x
+    (Cons h $ update t (i-1) x)
