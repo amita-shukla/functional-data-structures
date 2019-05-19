38e71b788517595e4bc6f28a2b128166fe278f20
amita-shukla <amitashukla0906@gmail.com>
2019-05-19 22:22:13 +0530

    format and add comments UnbalancedSet

--- src/DSA/Set/UnbalancedSet.hs
+++ src/DSA/Set/UnbalancedSet.hs
@@ -1,4 +1,8 @@
-{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, InstanceSigs #-}
+{-# LANGUAGE MultiParamTypeClasses #-} -- Set takes more then 1 type parameter
+{-# LANGUAGE FlexibleInstances #-} -- To allow Set instance have arbitrary nested types
+{-# LANGUAGE InstanceSigs #-} -- Allow putting instance  signatures in type instances
+  
+
 module DSA.Set.UnbalancedSet where
 
 import DSA.BST (Tree(..), insert)
@@ -14,10 +18,6 @@ instance Ord a => Set UnbalancedSet a where -- make UnbalancedSet an instance of
   insert :: a -> UnbalancedSet a -> UnbalancedSet a
   insert = DSA.BST.insert
   
---  member :: a -> UnbalancedSet a -> Bool
---  member a Leaf = False
---  member a (l b r) = if
-
   member :: a -> UnbalancedSet a -> Bool
   member a Leaf = False
   member a (Node l b r) = if a<b then member a l else if a>b then member a r else True
